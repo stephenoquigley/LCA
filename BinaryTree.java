ee9ac5fed348c1c1a342b18e3aeac81219671ff0
Stephen O'Quigley <stephenoquigley@yahoo.ie>
2018-10-01 23:07:08 +0100

    Tidying up class

--- BinaryTree.java
+++ BinaryTree.java
@@ -19,19 +19,14 @@ public class BinaryTree
         return findLCA(root, n1, n2); 
     } 
   
-    // This function returns pointer to LCA of two given 
-    // values n1 and n2. This function assumes that n1 and 
-    // n2 are present in Binary Tree 
+    
     Node findLCA(Node node, int n1, int n2) 
     { 
         // Base case 
         if (node == null) 
             return null; 
   
-        // If either n1 or n2 matches with root's key, report 
-        // the presence by returning root (Note that if a key is 
-        // ancestor of other, then the ancestor key becomes LCA 
-        if (node.data == n1 || node.data == n2) 
+                if (node.data == n1 || node.data == n2) 
             return node; 
   
         // Look for keys in left and right subtrees 
@@ -48,23 +43,3 @@ public class BinaryTree
         return (left_lca != null) ? left_lca : right_lca; 
     } 
     
-//    public static void main(String args[]) 
-//    { 
-//        BinaryTree tree = new BinaryTree(); 
-//        tree.root = new Node(1); 
-//        tree.root.left = new Node(2); 
-//        tree.root.right = new Node(3); 
-//        tree.root.left.left = new Node(4); 
-//        tree.root.left.right = new Node(5); 
-//        tree.root.right.left = new Node(6); 
-//        tree.root.right.right = new Node(7); 
-//        System.out.println("LCA(4, 5) = " + 
-//                            tree.findLCA(4, 5).data); 
-//        System.out.println("LCA(4, 6) = " + 
-//                            tree.findLCA(4, 6).data); 
-//        System.out.println("LCA(3, 4) = " + 
-//                            tree.findLCA(3, 4).data); 
-//        System.out.println("LCA(2, 4) = " + 
-//                            tree.findLCA(2, 4).data); 
-//    } 
-}
