570dface6b404df33ecfcf083c9a44db4445f67e
Stephen O'Quigley <stephenoquigley@yahoo.ie>
2018-10-12 12:24:53 +0100

    Testing atom ide

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,38 +1,38 @@
-class Node 
-{ 
-    int data; 
-    Node left, right; 
-  
-    public Node(int item) 
-    { 
-        data = item; 
-        left = right = null; 
-    } 
-} 
-public class BinaryTree 
+class Node
 {
-    Node root; 
-  
-    Node findLCA(int n1, int n2) 
-    { 
-        return findLCA(root, n1, n2); 
-    } 
-  
-    
-    Node findLCA(Node node, int n1, int n2) 
-    { 
-        if (node == null) 
-            return null; 
-  
-                if (node.data == n1 || node.data == n2) 
-            return node; 
-  
-        Node left_lca = findLCA(node.left, n1, n2); 
-        Node right_lca = findLCA(node.right, n1, n2); 
-  
-        if (left_lca!=null && right_lca!=null) 
-            return node; 
-  
-        return (left_lca != null) ? left_lca : right_lca; 
-    } 
-    
+  //Testing atom ide
+    int data;
+    Node left, right;
+
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+public class BinaryTree
+{
+    Node root;
+
+    Node findLCA(int n1, int n2)
+    {
+        return findLCA(root, n1, n2);
+    }
+
+
+    Node findLCA(Node node, int n1, int n2)
+    {
+        if (node == null)
+            return null;
+
+                if (node.data == n1 || node.data == n2)
+            return node;
+
+        Node left_lca = findLCA(node.left, n1, n2);
+        Node right_lca = findLCA(node.right, n1, n2);
+
+        if (left_lca!=null && right_lca!=null)
+            return node;
+
+        return (left_lca != null) ? left_lca : right_lca;
+    }
