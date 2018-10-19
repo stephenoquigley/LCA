54c3a484469e1056d350a8239e993cd0cd17c5cb
Stephen O'Quigley <stephenoquigley@yahoo.ie>
2018-10-19 11:59:30 +0100

    Adding getters and setters for marked

--- DirectedDFS.java
+++ DirectedDFS.java
@@ -50,7 +50,7 @@ public class DirectedDFS {
      * @throws IllegalArgumentException unless {@code 0 <= s < V}
      */
     public DirectedDFS(Digraph G, int s) {
-        marked = new boolean[G.V()];
+        setMarked(new boolean[G.V()]);
         validateVertex(s);
         dfs(G, s);
     }
@@ -64,18 +64,18 @@ public class DirectedDFS {
      *         for each vertex {@code s} in {@code sources}
      */
     public DirectedDFS(Digraph G, Iterable<Integer> sources) {
-        marked = new boolean[G.V()];
+        setMarked(new boolean[G.V()]);
         validateVertices(sources);
         for (int v : sources) {
-            if (!marked[v]) dfs(G, v);
+            if (!getMarked()[v]) dfs(G, v);
         }
     }
 
     private void dfs(Digraph G, int v) {
         count++;
-        marked[v] = true;
+        getMarked()[v] = true;
         for (int w : G.adj(v)) {
-            if (!marked[w]) dfs(G, w);
+            if (!getMarked()[w]) dfs(G, w);
         }
     }
 
@@ -88,7 +88,7 @@ public class DirectedDFS {
      */
     public boolean marked(int v) {
         validateVertex(v);
-        return marked[v];
+        return getMarked()[v];
     }
 
     /**
@@ -103,7 +103,7 @@ public class DirectedDFS {
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
-        int V = marked.length;
+        int V = getMarked().length;
         if (v < 0 || v >= V)
             throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
     }
@@ -113,11 +113,19 @@ public class DirectedDFS {
         if (vertices == null) {
             throw new IllegalArgumentException("argument is null");
         }
-        int V = marked.length;
+        int V = getMarked().length;
         for (int v : vertices) {
             if (v < 0 || v >= V) {
                 throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
             }
         }
     }
+
+	public boolean[] getMarked() {
+		return marked;
+	}
+
+	public void setMarked(boolean[] marked) {
+		this.marked = marked;
+	}
 }
